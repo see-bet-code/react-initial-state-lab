18af154001353c5218cbe10a82a4dea4a0eeeb29
see-bet-code <brittani.elan.taylor@gmail.com>
2020-11-18 08:44:15 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,19 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+  constructor(props) {
+    super()
+    this.state = { 
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render () {
+    return (
+      <h1>
+        {this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`}
+      </h1>
+    )
+  }
+}
